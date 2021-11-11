7d16e998e3f5ecb29a5c4f0fc08b3e080132e227
Andrew E. Falcon <andrew.falcon@trucesoftware.com>
2021-11-11 15:27:57 -0600

    Colocate py and pyi files

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     name='iterable_collections',
     version='0.1.6',
     packages=['iterable_collections'],
+    package_data={'iterable_collections': ['*.pyi']},
     license='MIT License',
     author='Allie Fitter',
     author_email='fitterj@gmail.com',
@@ -16,15 +17,6 @@ setup(
     long_description_content_type='text/x-rst',
     keywords=['collection', 'map', 'filter', 'pipeline', 'linq', 'scala'],
     url='https://github.com/alliefitter/iterable_collections',
-    data_files=[(
-        'lib/python{}.{}/site-packages/iterable_collections'.format(*sys.version_info[:2]),
-        [
-            "iterable_collections/collection.pyi",
-            "iterable_collections/strategy.pyi",
-            "iterable_collections/factory.pyi",
-            "iterable_collections/utils.pyi"
-        ]
-    )],
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
